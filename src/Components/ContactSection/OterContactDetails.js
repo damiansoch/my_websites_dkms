20924b53c2f817dde46a6dcb5925eb27ddd8e151
Damian Socha <damian.socha@amigo.ie>
2023-11-14 12:10:27 +0000

    changed other contact details

--- src/Components/ContactSection/OterContactDetails.js
+++ src/Components/ContactSection/OterContactDetails.js
@@ -1,12 +1,22 @@
-import FacebookMsg from './FacebookMsg';
-
 const OterContactDetails = ({ divHeight }) => {
   return (
     <div
       style={{ height: divHeight }}
-      className='shadow contactSingleContainer p-3'
+      className='shadow contactSingleContainer justify-content-evenly'
     >
-      <FacebookMsg />
+      <div>
+        <h4 className='title '>Other Contact:</h4>
+      </div>
+      <div>
+        <p>
+          <a href='mailto:	dkms.dev.personal@gmail.com?subject=Website%20Enquiry'>
+            Email: dkms.dev.personal@gmail.com
+          </a>
+        </p>
+        <p>
+          <a href='tel:0868406691'>Phone: 0868406691</a>
+        </p>
+      </div>
     </div>
   );
 };
